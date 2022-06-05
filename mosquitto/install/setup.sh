6d3b411d4d52d4cd31d1571bde0382a246f8db05
Agostinho Ramos <agostinhopina095@gmail.com>
2022-06-05 17:41:47 +0100

    Setting up mqtt

--- mosquitto/install/setup.sh
+++ mosquitto/install/setup.sh
@@ -4,6 +4,7 @@ sudo apt update && sudo apt upgrade -y
 
 hostname="127.0.0.1"
 username="mqttadmin"
+password="over224433"
 
 mqtt_port=1883
 mqtt_tls_port=8883
@@ -20,15 +21,15 @@ sudo rm -rf "/etc/mosquitto/tmp/"
 sudo rm /etc/mosquitto/ca_certificates/ca.crt
 sudo rm /etc/mosquitto/ca_certificates/server.crt
 sudo rm /etc/mosquitto/ca_certificates/server.key
-sudo apt-get purge --remove mosquitto*
+sudo apt-get purge --remove mosquitto* -y
+
 sleep 2
 
 sudo apt install mosquitto -y
-sudo systemctl status mosquitto
+# sudo systemctl status mosquitto
 sudo systemctl restart mosquitto
 
-echo "Please enter MQTT password of $username:";
-sudo mosquitto_passwd -c /etc/mosquitto/passwd $username
+echo -e "$password\n$password\n" | sudo mosquitto_passwd -c /etc/mosquitto/passwd $username
 sleep 1
 
 sudo touch /etc/mosquitto/conf.d/default.conf
@@ -44,28 +45,52 @@ if [ $enable_tls -ge 1 ]; then
     sudo mkdir "/etc/mosquitto/tmp/"
     cd /etc/mosquitto/tmp/
 
-    echo "1 - First create a key pair for the CA"
-    sudo openssl genrsa -des3 -out ca.key 2048
-    echo "2 - Create a certificate for the CA using the CA key"
-    sudo openssl req -new -x509 -days 1833 -key ca.key -out ca.crt
-    echo "3 - Create a server key pair for Broker"
+    COUNTRY="PT"
+    STATE="Lisbon"
+    LOCALITY="Lisbon"
+    ORG_NAME="IPG"
+    ORG_U_NAME="IPG"
+    DOMAIN="airva.local"
+    PASSWORD="over1234"
+
+    SUBJECT="/C=$COUNTRY/ST=$STATE/L=$LOCALITY/O=$ORG_NAME/OU=$ORG_U_NAME/CN=$DOMAIN"
+
+    # 1 - First create a key pair for the CA
+    sudo openssl genrsa -des3 -passout pass:$PASSWORD -out ca.key 2048
+
+    # 2 - Create a certificate for the CA using the CA key
+    sudo openssl req -passin pass:$PASSWORD -new -x509 -days 1833 -key ca.key -out ca.crt -subj "$SUBJECT"
+
+    # 3 - Create a server key pair for Broker
     sudo openssl genrsa -out server.key 2048
-    echo "4 - Create a certificate request .csr"
-    sudo openssl req -new -out server.csr -key server.key
-    echo "5 - Using CA key to verify and sign the server certificate"
-    sudo openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 360
+
+    # 4 - Create a certificate request .csr
+    sudo openssl req -new -key server.key -out server.csr -subj "$SUBJECT"
+
+    # 5 - Using CA key to verify and sign the server certificate
+    sudo openssl x509 -req -passin pass:$PASSWORD -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 360
     
     echo "Moving to ca_certificates folder"
     sudo mv ca.crt /etc/mosquitto/ca_certificates/
     sudo mv server.crt /etc/mosquitto/ca_certificates/
     sudo mv server.key /etc/mosquitto/ca_certificates/
     sudo chmod 777 -R /etc/mosquitto/ca_certificates
-
-    sudo echo -e "\n# TLS" >> /etc/mosquitto/conf.d/default.conf
-    sudo echo -e "listener $mqtt_tls_port" >> /etc/mosquitto/conf.d/default.conf
-    sudo echo -e "cafile /etc/mosquitto/ca_certificates/ca.crt\nkeyfile /etc/mosquitto/ca_certificates/server.key\ncertfile /etc/mosquitto/ca_certificates/server.crt\ntls_version tlsv1.2\n" >> /etc/mosquitto/conf.d/default.conf
+    {
+        echo ""
+        echo "# TLS"
+        echo "listener $mqtt_tls_port"
+        echo "cafile /etc/mosquitto/ca_certificates/ca.crt"
+        echo "keyfile /etc/mosquitto/ca_certificates/server.key"
+        echo "certfile /etc/mosquitto/ca_certificates/server.crt"
+        echo "tls_version tlsv1.2"
+    } >> /etc/mosquitto/conf.d/default.conf
 fi
-sudo echo -e "allow_anonymous true\npassword_file /etc/mosquitto/passwd" >> /etc/mosquitto/conf.d/default.conf
+
+{
+    echo ""
+    echo "allow_anonymous true"
+    echo "password_file /etc/mosquitto/passwd"
+} >> /etc/mosquitto/conf.d/default.conf
 
 if [ $enable_forwarding -ge 1 ]; then
     sudo ufw allow $mqtt_port/tcp
