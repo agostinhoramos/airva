00f5ca52ec79bbe2260f3b5beeb26634a23c275d
Agostinho Ramos <agostinhopina095@gmail.com>
2022-07-14 16:35:36 +0100

    Setting up new changes

--- poetry/setup.sh
+++ poetry/setup.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-sudo apt-get install -y python3-venv
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-source $HOME/.poetry/env
+version=$(eval "curl --version");
+if [ -z "$version" ]; then
+    sudo apt install -y curl;
+fi
+
+version=$(eval "python --version");
+if [ -z "$version" ]; then
+    alias python=python3;
+fi
+
+version=$(eval "poetry --version");
+if [ -z "$version" ]; then
+    sudo apt-get install -y python3-venv
+    curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+    source $HOME/.poetry/env
+else
+    echo "Poetry already installed :)"
+fi
