54c970dad23471de1e0dca59394af641a3031814
Agostinho Ramos <agostinhopina095@gmail.com>
2022-07-16 14:06:16 +0000

    Websocket issue

--- apache2/install/setup.sh
+++ apache2/install/setup.sh
@@ -20,6 +20,7 @@ sudo a2enmod deflate
 sudo a2enmod headers
 sudo a2enmod proxy
 sudo a2enmod proxy_http
+sudo a2enmod proxy_wstunnel
 sudo a2enmod proxy_ajp
 sudo a2enmod proxy_connect
 sudo a2enmod proxy_balancer
@@ -38,11 +39,20 @@ sudo chown $USER:$USER $CPATH
     echo "   ServerAlias $HOSTNAME"
     echo "   ServerAdmin webmaster@$HOSTNAME"
     echo "   RewriteEngine On"
+
     echo "   RewriteCond %{HTTP:Upgrade} =websocket [NC]"
     echo "   RewriteRule /(.*)            ws://127.0.0.1:$PORT/""$""1 [P,L]"
     echo "   RewriteCond %{HTTP:Upgrade} !=websocket [NC]"
     echo "   RewriteRule /(.*)            http://127.0.0.1:$PORT/""$""1 [P,L]"
-    echo "   ProxyPassReverse / http://127.0.0.1:$PORT/"
+    
+
+    echo "   <Proxy balancer://backend-cluster>"
+    echo "       BalancerMember http://127.0.0.1:$PORT/"
+    echo "   </Proxy>"
+    
+    echo "   ProxyPass / balancer://backend-cluster/"
+    echo "   ProxyPassReverse / balancer://backend-cluster/"
+    echo "   ProxyRequests off"
     echo "</VirtualHost>"
 } > $CONF_FILE
 
